e7144dea4bb9b20067b73278be806321af5a00ef
Ali Aydoğdu <86653103+aliaydogdu000@users.noreply.github.com>
2022-07-27 18:00:23 +0300

    assets, search page,settings page,theme added

--- flutter_weather/packages/meta_weather_api/lib/meta_weather_api.dart
+++ flutter_weather/packages/meta_weather_api/lib/meta_weather_api.dart
@@ -2,9 +2,3 @@ library meta_weather_api;
 
 export 'src/meta_weather_api_client.dart';
 export 'src/models/models.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
--- flutter_weather/packages/weather_repository/lib/weather_repository.dart
+++ flutter_weather/packages/weather_repository/lib/weather_repository.dart
@@ -2,9 +2,3 @@ library weather_repository;
 
 export 'src/models/model.dart';
 export 'src/weather_repository.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
